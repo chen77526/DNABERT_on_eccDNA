242724bf5322f9253c8b5432e3e80418584e5cb0
chen77526 <chen77526@yahoo.com.tw>
2023-02-21 21:02:30 +0800

    synchronize dataset generation

--- eccdna/label_generate.py
+++ eccdna/label_generate.py
@@ -20,8 +20,8 @@ def seq2kmer(seq, k):
 def mainf():
     with open ('./output/{}/label_{}.tsv'.format(args.data, args.length), 'w') as output_f1:
         with open ('./output/{}/label_{}_dev.tsv'.format(args.data, args.length), 'w') as output_f2:
-            with open('./output/{}/{}_positive_{}_limit.fa.out'.format(args.data, args.cellline, args.length), 'r') as input_f1:
-                with open('./output/{}/{}_negative_{}_limit.fa.out'.format(args.data, args.cellline, args.length), 'r') as input_f2:
+            with open('./output/{}/{}_{}_positive_{}_limit.fa.out'.format(args.data, args.data, args.cellline, args.length), 'r') as input_f1:
+                with open('./output/{}/{}_{}_negative_{}_limit.fa.out'.format(args.data, args.data, args.cellline, args.length), 'r') as input_f2:
                     ### split sequence into 6-mers
                     num1, num2 = 0, 0
                     positive, negative = [], []
@@ -35,14 +35,13 @@ def mainf():
                         num2 += 1
                     
                     ### randomly select sequence into validation set (20%) and add label on each sequence
-                    positive_dev = random.sample(positive, len(positive)//5)
-                    negative_dev = random.sample(negative, len(negative)//5)
+                    positive_dev, positive = positive[:len(positive) // 5], positive[len(positive) // 5:]
+                    negative_dev, negative = negative[:len(negative) // 5], negative[len(negative) // 5:]
+                    
                     for seq in positive_dev:
                         output_f2.writelines("%s\t%d\n" % (seq, 1))
-                        positive.remove(seq)
                     for seq in negative_dev:
                         output_f2.writelines("%s\t%d\n" % (seq, 0))
-                        negative.remove(seq)
 
                     for seq in positive:
                         output_f1.writelines("%s\t%d\n" % (seq, 1))
--- eccdna/limit_gen.sh
+++ eccdna/limit_gen.sh
@@ -6,18 +6,13 @@ showHelp() {
   message=$(cat <<- 'EOF'
 	Preprocess the eccdna .bed file to input .tsv file into DNABERT
 
-  Usage: Preprocess-DNABERT [OPTIONS] [-d <db_dir>] [-c <eccdna_bed>] [-t <datatype>] [-b <geno_bound>] [-r <geno_ref>] [-g <geno_gap>]
+  Usage: Preprocess-DNABERT [OPTIONS] [-d <db_dir>] [-t <datatype>]
     -d, --db_dir      db directory name
-    -c, --eccdna_bed  reference eccdna bed file
     -t, --datatype    species name / cell line name
-    -b, --geno_bound  reference genome boundary name
-    -r, --geno_ref    reference genome name
-    -g, --geno_gap    reference genome gap name
   
   Options:
-    -e, --ext         number of bp extended from eccdna center, Default=512
     -s, --seq_len     sequence length, Default=1024
-    -l, --limit       limit sequence length, Default=1000
+    -l, --limit       sequence length limit, Default=1000
     -h, --help        show this help messages
 
   Output: tsv file with binary label ( seq_len-5+1 columns ):
@@ -37,29 +32,19 @@ showHelp() {
 parseArgs() {
   while [[ "$#" > 0 ]]; do
   case $1 in
-    -e|--ext)         ext="$2";shift;shift;;
     -s|--seq_len)     seq_len="$2";shift;shift;;
-    -l|--limit)       limit="$2";shift;shift;;
     -d|--db_dir)      db_dir="$2";shift;shift;;
-    -c|--eccdna_bed)  eccdna_bed="$2";shift;shift;;
+    -l|--limit)       limit="$2";shift;shift;;
     -t|--datatype)    datatype="$2";shift;shift;;
-    -b|--geno_bound)  geno_bound="$2";shift;shift;;
-    -r|--geno_ref)    geno_ref="$2";shift;shift;;
-    -g|--geno_gap)    geno_gap="$2";shift;shift;;
     -h|--help)        showHelp;shift;shift;;
     *)                showHelp "Unknown parameter passed: $1";shift;shift;;
   esac; done
 
   # check / defaults
-  if [[ -z $ext ]]; then ext=512; fi
   if [[ -z $seq_len ]]; then seq_len=1024; fi
+  if [[ -z $db_dir ]]; then db_dir=human; fi
   if [[ -z $limit ]]; then limit=1000; fi
-  if [[ -z $db_dir ]]; then showHelp "db directory name (-d) not given."; fi
-  if [[ -z $eccdna_bed ]]; then showHelp "reference eccdna bed file (-c) not given."; fi
   if [[ -z $datatype ]]; then showHelp "species name / cell line name (-t) not given."; fi
-  if [[ -z $geno_bound ]]; then showHelp "reference genome boundary name (-b) not given."; fi
-  if [[ -z $geno_ref ]]; then showHelp "reference genome name (-r) not given."; fi
-  if [[ -z $geno_gap ]]; then showHelp "reference genome gap name (-g) not given."; fi
 }
 
 parseArgs "$@"
@@ -68,21 +53,6 @@ export eccdna_dir=eccdna_${datatype}_limit${limit}                      # eccdna
 #Change directory to where the script is
 cd "${0%/*}"
 
-### Acoording to sequence length contraint, check original .bed files and produce corresponding positive label .bed file
-### and sequence need to removed in order to generate a negative label .bed file
-python bed_limit.py --extend $ext --datatype $datatype --limit $limit --boundary ./genome/$db_dir/$geno_bound --gap ./genome/$db_dir/$geno_gap --eccdna ./db/$db_dir/$eccdna_bed --output ./db/$db_dir/
-
-### Generate negative label .bed file
-bedtools shuffle -i ./db/$db_dir/${datatype}_circleseq_eccdna_filt_uniq_seq_${seq_len}_${limit}.bed -g ./genome/$db_dir/$geno_bound -excl ./db/$db_dir/${datatype}_circleseq_eccdna_filt_uniq_excl_${seq_len}_${limit}.bed > ./db/$db_dir/${datatype}_circleseq_eccdna_filt_uniq_comp_${seq_len}_${limit}.bed
-echo "Finish generating negative label data"
-### Check if negative label .bed is intersect with positive label .bed
-bedtools intersect -a ./db/$db_dir/${datatype}_circleseq_eccdna_filt_uniq_comp_${seq_len}_${limit}.bed -b ./db/$db_dir/${datatype}_circleseq_eccdna_filt_uniq_excl_${seq_len}_${limit}.bed
-
-### Acquire its corresponding FASTA file
-bedtools getfasta -fi ./genome/$db_dir/$geno_ref -bed ./db/$db_dir/${datatype}_circleseq_eccdna_filt_uniq_seq_${seq_len}_${limit}.bed -fo ./output/$db_dir/${datatype}_positive_${seq_len}_limit.fa.out
-bedtools getfasta -fi ./genome/$db_dir/$geno_ref -bed ./db/$db_dir/${datatype}_circleseq_eccdna_filt_uniq_comp_${seq_len}_${limit}.bed -fo ./output/$db_dir/${datatype}_negative_${seq_len}_limit.fa.out
-echo "Finish converting bed files to fasta files"
-### Cut FASTA format into 6-mers and append its label to every sequence
 python label_generate.py --length $seq_len --data $db_dir --cellline $datatype
 echo "Finish generating 6-mers"
 ### (very important)Need to rename old label_${seq_len}_dev.tsv & label_${seq_len}.tsv & train.tsv & dev.tsv
